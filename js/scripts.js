f506d4beaea2e9339ba6dd538a8d1d9743bf74ab
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2020-10-28 19:30:27 -0300

    update JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 function openNav() {
-	
+	var x = document.getElementById("navigation")
+
+	if (x.className === "navigation") {
+		x.className += " menujs";
+	} else {
+		x.className = "navigation";
+	}
 }
